521c293cf5af9f1d35e47ac30d28972dde2b3287
陳若瑜 <b10902127@ntu.edu.tw>
2023-01-05 02:47:25 +0800

    deploy

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,13 +2,13 @@ import axios from "axios";
 
 const API_ROOT =
   process.env.NODE_ENV === "production"
-    ? "/api"
-    : "http://localhost:4000/api";
+    ? "/"
+    : "http://localhost:4000/";
 
 const WS_URL =
   process.env.NODE_ENV === "production"
     ? window.location.origin.replace(/^http/, "ws")
     : "ws://localhost:4000";
 
-// export default api = axios.create({ baseURL: API_ROOT });
-export default ws = new WebSocket(WS_URL);
+export default axios.create({ baseURL: API_ROOT });
+// export default ws = new WebSocket(WS_URL);
